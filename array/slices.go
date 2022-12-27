3eb6a49c80ceb0c38a1d29321da4640640802cba
harshika pinninti <pinnintiharshika@gmail.com>
2022-12-27 10:41:40 -0600

    updated

--- array/slices.go
+++ array/slices.go
@@ -9,10 +9,10 @@ func main() {
 	f[2] = 3
 	f[3] = 4
 	f[4] = 5
-	var y = []int{1, 2, 3, 4, 5, 54, 45, 34, 54, 43}
+	var g = []int{1, 2, 3, 4, 5, 54, 45, 34, 54, 43}
 	b := [5]int{2, 4, 6, 9, 10}
 	fmt.Printf("%T", b)
-	fmt.Printf("%T", y)
+	fmt.Printf("%T", g)
 	a := []int{2, 4, 6, 9, 10, 7, 5, 34, 345, 23}
 	fmt.Printf("%T", a)
 	m := []int{42, 43, 44, 45, 46}
@@ -20,10 +20,15 @@ func main() {
 	o := []int{44, 45, 46, 47, 48}
 	p := []int{43, 44, 45, 46, 47}
 	fmt.Println(m, n, o, p)
-	fmt.Println(y[1:6])
-	fmt.Println(y[2:7])
-	fmt.Println(y[3:8])
-	fmt.Println(y[4:9])
-	var x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
-
+	fmt.Println(a[1:6])
+	fmt.Println(a[2:7])
+	fmt.Println(a[3:8])
+	fmt.Println(a[4:9])
+	var x = []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	x = append(x, 52)
+	fmt.Println(x)
+	x = append(x, 53, 54, 55)
+	fmt.Print(x)
+	var y = []int{56, 57, 58, 59, 60}
+	x = append(x, y...)
 }
